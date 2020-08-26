79f868e20ef39ff4486b342706c9ea7dd16300ee
oØ.o <o0-o@dotdigital.digital>
2020-08-26 15:34:56 -0400

    Better printf format (no tab)

--- lib/orun/orun_hello-world.sh
+++ lib/orun/orun_hello-world.sh
@@ -2,23 +2,23 @@
 
 : "${VALS[0]:=Hello, world!}"
 
-printf '%s:\t' 'Shell'                                  &&
+printf '%-8s' 'Shell:'                                  &&
 source      "${__LIB_PATH}/out_print.sh"                &&
 
-printf '%s:\t' 'AWK'                                    &&
-printf '%s' "${VALS[@]-}" |
+printf '%-8s' 'AWK:'                                    &&
+printf '%-8s' "${VALS[@]-}" |
   awk -f      "${__LIB_PATH}/out_print.awk"             &&
 
-printf '%s:\t' 'TCL'                                    &&
+printf '%-8s' 'TCL:'                                    &&
 tclsh       "${__LIB_PATH}/out_print.tcl" "${VALS[@]-}" &&
 
-printf '%s:\t' 'Perl'                                   &&
+printf '%-8s' 'Perl:'                                   &&
 perl        "${__LIB_PATH}/out_print.pl"  "${VALS[@]-}" &&
 
-printf '%s:\t' 'Python'                                 &&
+printf '%-8s' 'Python:'                                 &&
 "${PYTHON}" "${__LIB_PATH}/out_print.py"  "${VALS[@]-}" &&
 
-printf '%s:\t' 'Ruby'                                   &&
+printf '%-8s' 'Ruby:'                                   &&
 ruby        "${__LIB_PATH}/out_print.rb"  "${VALS[@]-}" ||
 
 return 1

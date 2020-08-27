ec986ab0f5dc72e5d1597e734b8c4bb39d71371d
oØ.o <o0-o@dotdigital.digital>
2020-08-26 21:34:39 -0400

    handle exit 100 on yum and dnf

--- lib/orun/sw_check-update_rhel.sh
+++ lib/orun/sw_check-update_rhel.sh
@@ -1,10 +1,16 @@
 # Check for software updates on Red Hat, CentOS or Fedora Linux
 
 # Fedora and recent versions of CentOS/RHEL
-sudo dnf check-update ||
+{ sudo dnf check-update ||
+  # exits 100 when updates are available
+  [ "$?" -eq '100' ]
+} ||
 
 # Earlier versions of CentOS/RHEL
-sudo yum check-update ||
+{ sudo yum check-update ||
+  # exits 100 when updates are available
+  [ "$?" -eq '100' ]
+} ||
 
 return 1
 

044aa8b8924ee6d6caae17687d72b7fe7dbce970
oØ.o <o0-o@dotdigital.digital>
2020-10-22 14:43:42 -0400

    update comments

--- lib/orun/auth_1password.sh
+++ lib/orun/auth_1password.sh
@@ -1,14 +1,17 @@
-# LOGIN   To 1password.com $domain with $password, $email and $secret.
-#         If $domain has been logged in previously on the host, only
-#         $account and $password are necessary.
+# LOGIN   To 1password.com $domain with $token or $password, $email and
+#         $secret.
+#         If $domain has been logged in previously on the host, $account
+#         may be used in place of $email and $secret.
 # STDOUT  1Password session token
 ########################################################################
 
 # Try token
 printf  ' '                       |
+# Sending empty password avoids TTY prompt in case of invalid token
   op    signin                    \
         --raw                     \
         --session   "${token}"    ||
+# op signin -r --session "$token"
 
 # Fallback to full login
 printf  '%s'                      \

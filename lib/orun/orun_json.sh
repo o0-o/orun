8b6961744a25291ae4d6f245e0cc0448577c628a
oØ.o <o0-o@dotdigital.digital>
2020-11-09 12:49:19 -0500

    add missing json wrapper

---
+++ lib/orun/orun_json.sh
@@ -0,0 +1,9 @@
+# STDOUT  Value found traversing positional arguments as fields, except
+#         the last argument which is a JSON string or file
+########################################################################
+
+"${_script_wrapper[@]}"                 ||
+
+return 1
+
+return 0
